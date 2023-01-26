1ae130a2063eed0edc7f406428fe176b56f414aa
Tymur <nicktimur228@gmail.com>
2023-01-26 17:23:54 +0200

    Help update

--- Tree/help.h
+++ Tree/help.h
@@ -9,14 +9,14 @@
 #define help_h
 
 #define HELP "tree options:\n" \
-"-p             Print permissions of each file or directory.\n" \
-"--permissions  The same as the -p option.\n" \
-"-t <length>    The maximum length for filenames. Files with the name length exceeding the value will be truncated.\n" \
-"-e             Print filetype of each file or directory in the form of extension (e.g. png).\n" \
-"-u             Print filetype of each file or directory in the form of uti (e.g. public.png).\n" \
-"-c             Colorful option. To control the order of colors, set the TREE_COLORS environment variable accordingly. An example might be \"74;84\". Here 74 and 84 are one of the 256 colors. The maximum number of colors is 10. If the value ends with an equal sign (\"=\"), the last color will be repeated as many times as needed, otherwise the whole sequence will be repeated.\n" \
-"-s             Print size of each file or directory.\n" \
-"--size         The same as the -s option.\n" \
-"-d             Debug. Prints the list of command line arguments before going down the directory.\n"
+"-p             Prints the permissions of each entry\n" \
+"--permissions  The same as the -p option\n" \
+"-t <length>    Limits the length (in UTF-16 units) of filenames. Filenames longer than the specified value will be truncated\n" \
+"-e             Prints the filetype of each entry in the form of extension. E.g. png\n" \
+"-u             Prints the filetype of each entry in the form of UTI (Uniform Type Identifier). E.g. public.png\n" \
+"-c             Applies a different branch color on every level. You can specify up to 10 colors (values in the range 0-255) one by one separated by semicolon (\";\") in TREE_COLORS. If the number of levels is greater than the number of colors, the value of the environment variable must end with an equal sign (\"=\") to repeat only the last color, otherwise the whole sequence of colors will be repeated\n" \
+"-s             Prints the size of each entry\n" \
+"--size         The same as the -s option\n" \
+"-d             Debug. Prints the list of command line arguments before going down the directory\n"
 
 #endif /* help_h */
