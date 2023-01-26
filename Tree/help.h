a7e161aaa58ba2247a52ddbea18b54a587cfb068
Tymur <nicktimur228@gmail.com>
2023-01-26 22:05:17 +0200

    readme update

--- Tree/help.h
+++ Tree/help.h
@@ -14,7 +14,7 @@
 "-t <length>    Limits the length (in UTF-16 units) of filenames. Filenames longer than the specified value will be truncated\n" \
 "-e             Prints the filetype of each entry in the form of extension. E.g. png\n" \
 "-u             Prints the filetype of each entry in the form of UTI (Uniform Type Identifier). E.g. public.png\n" \
-"-c             Applies a different branch color on every level. You can specify up to 10 colors (values in the range 0-255) one by one separated by semicolon (\";\") in TREE_COLORS. If the number of levels is greater than the number of colors, the value of the environment variable must end with an equal sign (\"=\") to repeat only the last color, otherwise the whole sequence of colors will be repeated\n" \
+"-c             Applies a different branch color on each level. You can specify up to 10 colors (values in the range 0-255) one by one separated by semicolon (\";\") in TREE_COLORS. If the number of levels is greater than the number of colors, the value of the environment variable must end with an equal sign (\"=\") to repeat only the last color, otherwise the whole sequence of colors will be repeated\n" \
 "-s             Prints the size of each entry\n" \
 "--size         The same as the -s option\n" \
 "-d             Debug. Prints the list of command line arguments before going down the directory\n"
